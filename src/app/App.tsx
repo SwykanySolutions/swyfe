89f84f770e8bac8dfba3d565c51d80b71156e7ff
Ernani Ávila <ernanicavilaneto@gmail.com>
2023-11-09 20:15:35 -0300

    fix dark mode

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,38 +1,31 @@
-"use client"
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { useEffect, useState, useRef } from 'react'
-import ColorChanger from '@/components/ColorChanger'
+'use client';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { useEffect, useState } from 'react';
+import ColorChanger from '@/components/ColorChanger';
 
-const inter = Inter({ subsets: ['latin'] })
-export default function App({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    const [thema, setThema ] = useState("auto");
+const inter = Inter({ subsets: ['latin'] });
+export default function App({ children }: any) {
+	const [thema, setThema] = useState('auto');
 
-    const the = useRef(thema);
-    const setThe = useRef(setThema);
+	useEffect(() => {
+		if (thema == 'auto') {
+			if (window.matchMedia('(preferences-color-scheme: dark)').matches) {
+				localStorage.setItem('theme', 'dark');
+				setThema('dark');
+			} else {
+				localStorage.setItem('theme', 'light');
+				setThema('light');
+			}
+		} // eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-    useEffect(() => {
-        if (thema == "auto") {
-            if (window.matchMedia("(preferences-color-scheme: dark)").matches) {
-                localStorage.setItem("theme", "dark");
-                setThema("dark");
-            } else {
-                localStorage.setItem("theme", "light");
-                setThema("light");
-            }
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
-
-    return (
-      <html lang="pt-br" data-mode={thema}>
-        <body className={inter.className}>{children}
-            <ColorChanger colorTheme={thema} />
-        </body>
-      </html>
-    )
-  }
+	return (
+		<html lang="pt-br" data-mode={thema}>
+			<body className={inter.className}>
+				<ColorChanger colorTheme={thema} fun={setThema} />
+				{children}
+			</body>
+		</html>
+	);
+}
